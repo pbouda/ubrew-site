deefd1e284de8a3557c321bb3906bf9b8a68523d
Peter Bouda <pbouda@cidles.eu>
2015-03-20 17:09:10 +0000

    Changed title.

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Peter Bouda'
 SITENAME = 'HaQt'
-SITESUBNAME = 'Sei kreativ'
+SITESUBNAME = 'Ungleich kreativ'
 SITESUBTITLE = 'Kreativ und Produktiv Programmieren mit Qt'
 SITEURL = ''
 
